c5f359b26356c44b5f21d268cff531daab5e62f5
Sam <vyan452000@gmail.com>
2024-03-20 00:15:30 +0800

    edit base url in app.jsx

--- ierg4210hw/client/src/App.jsx
+++ ierg4210hw/client/src/App.jsx
@@ -9,7 +9,7 @@ import ViewProduct from "./pages/view-product/ViewProduct"
 // import EditViewProduct from "./pages/view-product/EditViewProduct"
 import AddCatagory from "./pages/add-catagory/AddCatagory"
 
-axios.defaults.baseURL = "http://s15.ierg4210.ie.cuhk.edu.hk:4000"
+axios.defaults.baseURL = "https://s15.ierg4210.ie.cuhk.edu.hk/api"
 // axios.defaults.baseURL = "http://localhost:4000"
 axios.defaults.withCredentials = true
 
